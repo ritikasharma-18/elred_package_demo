26c446c43ed86c5847a1b9e01cb81e4d3e8530ed
ritika-1826 <ritika@elred.io>
2022-12-16 13:47:46 +0530

    added text color for colored container package

--- lib/colored_container.dart
+++ lib/colored_container.dart
@@ -6,15 +6,19 @@ import 'package:flutter/material.dart';
 class ColoredContainer extends StatelessWidget {
 
   final Color containerColor;
+  final Color textColor;
 
-  const ColoredContainer({super.key, required this.containerColor});
+  const ColoredContainer({super.key, required this.containerColor, required this.textColor});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 200,
-      width: 200,
-      color: containerColor,
+    return Center(
+      child: Container(
+        height: 200,
+        width: 200,
+        color: containerColor,
+        child: Text("Hello", style: TextStyle(fontSize: 20, color: textColor),),
+      ),
     );
   }
 }
